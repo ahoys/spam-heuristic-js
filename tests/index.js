ba4046810f970769fef8f9f25bf869a447801553
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-08 22:29:01 +0200

    Minor test preparations.

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
-const spamDetect = require('../index');
+const heuristic = require('../index');
 
 /**
  * Should return probability and severity of spam from a string.
  * @param test
  */
 exports.getSpamProbability_String = function(test) {
-    const value = spamDetect.getSpamProbability('msg');
+    const value = heuristic.getSpamProbability('msg');
     test.equal(value.probability, 0);
     test.equal(value.severity, 0);
     test.expect(2);
@@ -17,10 +17,10 @@ exports.getSpamProbability_String = function(test) {
  * @param test
  */
 exports.getSpamProbability_Array = function(test) {
-    const valueArr = spamDetect.getSpamProbability(['msg']);
+    const valueArr = heuristic.getSpamProbability(['msg']);
     test.equal(valueArr.probability, 0);
     test.equal(valueArr.severity, 0);
-    const valueArr2 = spamDetect.getSpamProbability(['msg', 'msg']);
+    const valueArr2 = heuristic.getSpamProbability(['msg', 'msg']);
     test.equal(valueArr2.probability, 0);
     test.equal(valueArr2.severity, 0);
     test.expect(4);
