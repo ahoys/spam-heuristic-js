651187c251d225fca1f7f7dce476726813dac5ff
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-20 21:18:12 +0200

    binded eventMessage to event with heuristics

--- src/class.EventMessage.js
+++ src/class.EventMessage.js
@@ -1,4 +1,5 @@
 const Event = require('./class.Event');
+const Emphasis = require('./emphasis/event.json');
 module.exports = class EventMessage extends Event {
 
     /**
@@ -13,5 +14,13 @@ module.exports = class EventMessage extends Event {
         super();
         this.msgValue = String(msgValue);
         this.msgWords = this.msgValue.split(" ");
+
+        // Run heuristics for the value.
+        this.shortWordsPercentage = super.getPercentageOfShortWords(
+            this.msgWords, Emphasis.EventMessage.short_word_limit);
+        this.longWordsPercentage = super.getPercentageOfLongWords(
+            this.msgWords, Emphasis.EventMessage.long_word_limit);
+        this.repetitiveCharsPercentage = super.getRepetitiveCharsPercentage(
+            this.msgValue);
     }
 };
