950b4e95f908cfc462b27362727e46ff0b26eb37
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-20 21:59:35 +0200

    Clean-up.

--- src/class.EventMessage.js
+++ src/class.EventMessage.js
@@ -14,25 +14,26 @@ module.exports = class EventMessage extends Event {
         super();
         this.msgValue = String(msgValue);
         this.msgWords = this.msgValue.split(" ");
-        let violationsTotal = 3;
-        let violations = 0;
+
         // Run heuristics for the value.
         const heuristicPercentages = [];
         heuristicPercentages.push(super.getPercentageOfShortWords(
             this.msgWords, Emphasis.EventMessage.short_word_limit));
-        if (heuristicPercentages[0] > 10) violations++;
         heuristicPercentages.push(super.getPercentageOfLongWords(
             this.msgWords, Emphasis.EventMessage.long_word_limit));
-        if (heuristicPercentages[1] > 10) violations++;
         heuristicPercentages.push(super.getRepetitiveCharsPercentage(
             this.msgValue));
-        if (heuristicPercentages[2] > 10) violations++;
+
         // Analyse the results.
+        let violations = 0;
         let heuristicSum = 0;
-        heuristicPercentages.forEach((result) => {
-            heuristicSum = heuristicSum + result;
+        heuristicPercentages.forEach((percentage) => {
+            if (percentage > 10) violations++;
+            heuristicSum = heuristicSum + percentage;
         });
-        super.certainty = (violations / violationsTotal) * 100;
+
+        // Save the results.
+        super.certainty = (violations / heuristicPercentages.length) * 100;
         super.severity = (heuristicSum / heuristicPercentages.length) / 10;
     }
 };
