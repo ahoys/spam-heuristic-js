26d44bffaf0c3641d861a3d9f08710e07be4c6fa
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-02-25 10:19:06 +0200

    More tests.

--- tests/test.EventMessage.js
+++ tests/test.EventMessage.js
@@ -1,13 +1,6 @@
 // nodeunit tests/test.EventMessage.js
 const EventMessage = require('../src/class.EventMessage');
 
-// Samples ------------------------------------
-
-const arr0 = ['a'];
-const arr1 = ['aaa', 'bbb'];
-const arr2 = ['a', 'bb', 'ccc'];
-const arr3 = ['a%', ')=¤"/', '123', '  ', 12345];
-
 // Tests --------------------------------------
 
 exports.initialize = function (test) {
@@ -15,10 +8,82 @@ exports.initialize = function (test) {
     const message = eventMessageObj.message;
     const certainty = eventMessageObj.certainty;
     const severity = eventMessageObj.severity;
-    console.log(message, certainty, severity);
     test.equal(message, 'test');
     test.equal(certainty, 0);
     test.equal(severity, 0);
     test.expect(3);
+    test.done();
+};
+
+exports.spamMessage_repeatingWords = function (test) {
+    const eventMessageObj = new EventMessage('test test test test test test');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 0);
+    test.equal(severity, 0);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_shortWords0 = function (test) {
+    const eventMessageObj = new EventMessage('t h i s i s s p a m ee hh h');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 33);
+    test.equal(severity, 3);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_shortWords1_falseFlag = function (test) {
+    const eventMessageObj = new EventMessage('oh my how odd is it spam rly im not spamming');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 33);
+    test.equal(severity, 3);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_longWords0 = function (test) {
+    const eventMessageObj = new EventMessage('thesewordsareartificiallylongandimjustrepeatingthem ' +
+        'thesewordsareartificiallylongandimjustrepeatingthem');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 33);
+    test.equal(severity, 3);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_longWords1_falseFlag = function (test) {
+    const eventMessageObj = new EventMessage('The manifestation of the existential paradigm is ' +
+        'infinitesimally larger than the exponentially evolved humanistic peon; indeed this precept ' +
+        'is fundamentally beyond the cognisance of any finite mind.');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 33);
+    test.equal(severity, 1);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_repeatingChars0 = function (test) {
+    const eventMessageObj = new EventMessage('aaaaaaaaaaaaaaaaaaaaaaaaaa!');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 67);
+    test.equal(severity, 6);
+    test.expect(2);
+    test.done();
+};
+
+exports.spamMessage_repeatingChars1 = function (test) {
+    const eventMessageObj = new EventMessage('aaaaaaaAAAaaAAa    aaaaaaaaaa!');
+    const certainty = eventMessageObj.certainty;
+    const severity = eventMessageObj.severity;
+    test.equal(certainty, 67);
+    test.equal(severity, 4);
+    test.expect(2);
     test.done();
 };
