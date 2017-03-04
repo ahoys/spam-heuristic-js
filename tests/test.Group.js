39abd61da150a395e1c86c27b6f1a0bffd6c19d8
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-03-04 13:59:26 +0200

    More fuller setRecord testing.

--- tests/test.Group.js
+++ tests/test.Group.js
@@ -5,13 +5,13 @@ const Immutable = require('immutable');
 
 const gId0 = 'g123';
 const sId0 = 's123';
+const sId1 = 321;
 const eventMessage0 = {type: 'eventMessage', value: 'msg 123 ! " / 3'};
+const eventMessage1 = {type: 'eventMessage', value: 'repeat repeat repeat repeat repeat repeat repeat repeat'};
 
 // Tests --------------------------------------
 
 exports.setRecord = function (test) {
-
-
     const groupObj = new Group(gId0);
 
     let actual = groupObj.id;
@@ -27,10 +27,17 @@ exports.setRecord = function (test) {
     actual = groupObj.suspects;
     test.equal(actual.get(sId0).id, sId0, '2');
 
-    test.expect(3);
+    groupObj.setRecord(sId0, eventMessage1);
+    groupObj.setRecord(sId1, eventMessage0);
+
+    actual = groupObj.records;
+    test.equal(actual.size, 3, '3');
+    test.equal(actual.get(2).sId, sId1, '4');
+
+    actual = groupObj.suspects;
+    test.equal(actual.size, 2, '5');
+    test.equal(actual.get(sId1).id, sId1, '6');
+
+    test.expect(7);
     test.done();
 };
-
-// exports.getSuspectAnalysis = function (test) {
-//
-// };
